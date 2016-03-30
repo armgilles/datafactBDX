f6f7b1c9e223982efdf244f7b4aa4b4d1f8fff21
Armand Gilles <arm.gilles@gmail.com>
2016-03-30 12:01:49 +0200

    Create accident bike by city script

---
+++ accidents_bike.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 30 10:58:12 2016
+
+@author: babou
+"""
+
+import pandas as pd
+
+accidents = pd.read_csv('data/accidents-corporels-de-la-circulation-en-france.csv', sep=";")
+
+
+# Il n'éxiste pas de données sur le nombre d'accident sur les vélos en France.
+# Nous prennons donc l'hypothèse que si l'accident se déroule sur une piste cyclable,
+# un vélo y est impliqué.
+
+# Uniquement accident sur piste cyclable / bande cyclable
+bike = accidents[accidents["Voie spéciale"].str.contains("cyclable", na=False)]
+
+# On regroupe le nombre d'accident par ville
+grp_insee = bike.groupby(['Code Insee']).size().reset_index()
+grp_insee.columns = ['COM', 'accident_velo']
+
+# export
+grp_insee.to_csv('data/accident_bike_com.csv', index=False)
